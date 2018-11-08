be49076def576c47cb7545d52d4c37fe75c5d3e1
abhilmohan <abhilmohan@gmail.com>
2018-11-08 17:16:47 +0530

    first commit

--- src/NthrootCalculator.java
+++ src/NthrootCalculator.java
@@ -1,7 +1,7 @@
 public class NthrootCalculator {
 
     // x is the base and n is the power
-
+    //  n root of x => x^(1/n) => e^(lnx/n)
 
     public double nroot(Double base, Double n){
         double result = Math.pow(Math.E ,Math.log(base)/n);
